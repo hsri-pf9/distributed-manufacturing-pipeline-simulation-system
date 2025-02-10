68af6c210008e1f3cdd9bf2f987beb701292d8d5
Harsh Srivastava <harshsrivastava@pf9s-MacBook-Pro.local>
2025-02-10 17:53:49 +0530

    added codes of database connection and table creation

---
+++ internal/adapters/secondary/db.go
@@ -0,0 +1,30 @@
+package secondary
+
+import (
+	"log"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+	"github.com/hsri-pf9/distributed-manufacturing-pipeline-simulation-system/internal/core/domain"
+)
+
+var DB *gorm.DB
+
+func InitDatabase() {
+	dsn := os.Getenv("SUPABASE_DB_URL")
+	if dsn == "" {
+		log.Fatal("SUPABASE_DB_URL environment variable is not set")
+	}
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to Supabase database: %v", err)
+	}
+
+	if err := DB.AutoMigrate(&domain.PipelineExecution{}, &domain.ExecutionLog{}); err != nil {
+		log.Fatalf("Database migration failed: %v", err)
+	}
+	
+
+}
---
+++ internal/core/domain/models.go
@@ -0,0 +1,27 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// PipelineExecution stores pipeline execution details
+type PipelineExecution struct {
+	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	PipelineID uuid.UUID `gorm:"type:uuid;not null"`
+	Status     string    `gorm:"type:varchar(50);not null"`
+	CreatedAt  time.Time `gorm:"autoCreateTime"`
+	UpdatedAt  time.Time `gorm:"autoUpdateTime"`
+}
+
+// ExecutionLog stores logs related to pipeline execution
+type ExecutionLog struct {
+	ID         uuid.UUID `gorm:"type:uuid;default:uuid_generate_v4();primaryKey"`
+	StageID    uuid.UUID `gorm:"type:uuid;not null"`
+	PipelineID uuid.UUID `gorm:"type:uuid;not null"`
+	Status     string    `gorm:"type:varchar(50);not null"`
+	ErrorMsg   string    `gorm:"type:text"`
+	Timestamp  time.Time `gorm:"autoCreateTime"`
+}
+
